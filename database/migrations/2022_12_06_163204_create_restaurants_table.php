5cf76b6b3f497471f4988de1703dc9c940e31cd5
alemonacoo <ale.monaco@outlook.com>
2022-12-06 16:05:02 +0100

    changing original migration

---
+++ database/migrations/2022_12_06_163204_create_restaurants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->string('p_iva', 11)->unique()->nullable(false);
+            $table->string('slug')->unique();
+            $table->string('image')->nullable(true);
+            $table->string('address')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
