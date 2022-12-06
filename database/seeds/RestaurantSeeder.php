5afa0d4b1e6f68dd4540aefdff7bdc5a8200d519
alemonacoo <ale.monaco@outlook.com>
2022-12-06 13:11:00 +0100

    Seeding 20 random restaurants

--- database/seeds/RestaurantSeeder.php
+++ database/seeds/RestaurantSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Restaurant;
+use Illuminate\Support\Str;
 
 class RestaurantSeeder extends Seeder
 {
@@ -12,5 +14,33 @@ class RestaurantSeeder extends Seeder
     public function run()
     {
         //
+        for($i = 1; $i<=10; $i++){
+            $restaurant = new Restaurant();
+            $restaurant->name = 'Pizzeria n.' . $i;
+            $restaurant->p_iva = '000000000' . $i;
+            $restaurant->address = 'Via dei Mille n.' . $i;
+
+            $slug = Str::slug($restaurant->name);
+
+            //slug di partenza 
+            $slug_base = $slug;
+
+            $counter = 1;
+
+            //cerca un post che abbia slug uguale a slug appena creato 
+            $existingRestaurant = Restaurant::where('slug', $slug)->first();
+
+            //fintanto che esiste un post con slug selezionato  
+            while($existingRestaurant){
+                // incrementiamo slug con counter 
+                $slug = $slug_base . '_' . $counter;
+                $existingRestaurant = Restaurant::where('slug', $slug)->first();
+                $counter++;
+            }
+
+            $restaurant->slug = $slug;
+
+            $restaurant->save();
+        }
     }
 }
