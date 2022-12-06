ec37ea257eb210517e4a7500b7b7f07b7dad7ff0
alemonacoo <ale.monaco@outlook.com>
2022-12-06 12:31:30 +0100

    modifying dashboard + welcome

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/back.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -23,12 +23,10 @@
 
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark flex-md-nowrap p-0">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Boolpress</a>
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Deliveboo</a>
         <ul class="navbar-nav px-3 ml-auto">
             <li class="nav-item">
                 <a class="nav-link" href="/">
-                     
-                    
                     Visita il sito
                 </a>
             </li>
@@ -58,11 +56,11 @@
                                     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                                     <polyline points="9 22 9 12 15 12 15 22"></polyline>
                                 </svg>
-                                Dashboard
+                                Ristorante
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link">
+                            <a class="nav-link" >
                                 {{-- href="{{ route('admin.posts.index') }}" --}}
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -73,7 +71,7 @@
                                     <line x1="16" y1="17" x2="8" y2="17"></line>
                                     <polyline points="10 9 9 9 8 9"></polyline>
                                 </svg>
-                                Posts
+                                Menu
                             </a>
                         </li>
                         <li class="nav-item">
@@ -86,7 +84,7 @@
                                     <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
                                     <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
                                 </svg>
-                                Users
+                                Categorie
                             </a>
                         </li>
                         <li class="nav-item">
@@ -99,21 +97,7 @@
                                     <line x1="9" y1="20" x2="15" y2="20"></line>
                                     <line x1="12" y1="4" x2="12" y2="20"></line>
                                 </svg>
-                                Categories
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" >
-                                {{-- href="{{ route('admin.tags.index') }}" --}}
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                                    <path
-                                        d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z">
-                                    </path>
-                                    <line x1="7" y1="7" x2="7" y2="7"></line>
-                                </svg>
-                                Tags
+                                Ordini
                             </a>
                         </li>
                     </ul>
