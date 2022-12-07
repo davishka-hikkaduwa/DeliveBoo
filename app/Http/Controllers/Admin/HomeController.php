2ed0bb5e359cea19563875b46134fb8b59a969d1
alemonacoo <ale.monaco@outlook.com>
2022-12-07 12:01:56 +0100

    redirecting home controller to restaurants index

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
 {
     //
     public function index(){
-        return view('admin.home');
+        return redirect()->route('admin.restaurants.index');
     }
 }
