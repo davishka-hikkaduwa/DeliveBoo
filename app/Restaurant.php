d0815ccf885d121de2e9d13af1816147f75e26e3
alemonacoo <ale.monaco@outlook.com>
2022-12-07 10:59:35 +0100

    adding connections in Restaurant + User Models

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -8,4 +8,8 @@ class Restaurant extends Model
 {
     //
     protected $fillable = ['name', 'p_iva', 'slug', 'address', 'image'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
